11ad9869aa016643567ae0f60a17787ce6873ad6
Sparks727 <alex@omnilocal.co>
2024-05-15 19:26:02 -0400

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,114 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  XIcon,
+} from '@/components/SocialIcons'
+import portraitImage from '@/images/portrait.jpg'
+
+function SocialLink({ className, href, children, icon: Icon }) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export const metadata = {
+  title: 'About',
+  description:
+    'I’m Alex Sparks. I live in Clearwater, FL, where I design the future.',
+}
+
+export default function About() {
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+        <div className="lg:pl-20">
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src={portraitImage}
+              alt=""
+              sizes="(min-width: 1024px) 32rem, 20rem"
+              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+            I’m Alex Sparks and Im helping design the
+            future.
+          </h1>
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>
+            I have a deep passion for technology and a lifelong curiosity about how computers tick.
+            My fascination with technology began at a young age, fueled by gaming systems like the Nintendo 64 and Gameboys.
+            In my free time as a kid I would disemble & help fix Microsoft system hardware’s such as consoles and controllers.
+            </p>
+            <p>
+              While my upbringing involved frequent moves, I'm happy to call Clearwater, FL home. Love this area and couldn't see my self living anywhere else.
+              When I'm not diving into the world of technology, I'm planning my next adventure. 
+              Now a seasoned traveler, I have explored almost 20 states in the US and ventured abroad to destinations like Netherlands, Germany, and Puerto Rico.
+              At home, I enjoy family time with my partner Jordan, son Easton, and our canine companions, Balto (a Blue Heeler) and Kylo (a Blue Heeler/Staffordshire Terrier mix).
+            </p>
+            <p>
+              Currently, I am developing OmniLocal, a local marketing & reputation software  
+              that greatly simplifies and reduces the number of platforms that marketing agencies and small business owners have to use for local SEO.
+            </p>
+            <p>
+            Spearheading the implementation of cutting-edge features for businesses of all sizes, 
+            I possess a deep understanding of emerging business software solutions and extensive experience managing diverse marketing advertising systems.
+            My team has empowered over hundreds of local businesses to thrive by optimizing their online presence for customer acquisition through search engines and phone calls. 
+            Our proven track record of success spans various industries including realtors, lawyers, contractors, dentists, and more.
+            </p>
+          </div>
+        </div>
+        <div className="lg:pl-20">
+          <ul role="list">
+            <SocialLink href="https://www.facebook.com/OmniLocalCo/" icon={XIcon}>
+              Follow on Facebook
+            </SocialLink>
+            <SocialLink href="https://www.youtube.com/@OmniLocalai" icon={InstagramIcon} className="mt-4">
+              Subscribe to Channel
+            </SocialLink>
+            <SocialLink href="https://github.com/Sparks727/" icon={GitHubIcon} className="mt-4">
+              Follow on GitHub
+            </SocialLink>
+            <SocialLink href="https://www.linkedin.com/in/alex-sparks-%F0%9F%9A%80-41966a247/" icon={LinkedInIcon} className="mt-4">
+              Follow on LinkedIn
+            </SocialLink>
+            <SocialLink
+              href="mailto:sparksstack@gmail.com"
+              icon={MailIcon}
+              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+            >
+              sparksstack@gmail.com
+            </SocialLink>
+          </ul>
+        </div>
+      </div>
+    </Container>
+  )
+}
---
+++ src/app/travel/page.jsx
@@ -0,0 +1,42 @@
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function ToolsSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export const metadata = {
+  title: 'Travel',
+  description: 'Places I have traveled to',
+}
+
+export default function Travel() {
+  return (
+    <SimpleLayout
+      title="Places I have traveled to"
+      intro="Visting every state within The United States has always been a dream for me! Check out my progress below."
+    >
+      <div className="space-y-20">
+  <iframe src="https://visitedplaces.com/embed/?map=usa&projection=geoAlbersUsa&theme=dark-blue&water=0&graticule=0&names=1&duration=2000&placeduration=100&slider=0&autoplay=0&autozoom=none&autostep=0&home=US&places=~US-TX_US-AL_US-FL.41b975_US-GA_US-SC_US-NC_US-TN_US-KY_US-IN_US-IL_US-OH_US-MD_US-VA_US-PA_US-NJ_US-WV_US-DE_US-NV_US-CO_US-ME_US-ME%22%20style=%22width:%20100%;%20height:%20600px;" width="100%" height="500"></iframe>
+</div>
+    </SimpleLayout>
+  )
+}
