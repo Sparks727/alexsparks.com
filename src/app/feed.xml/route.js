c9c01c6b983467e8454942180416deed056efe90
Sparks727 <149076502+Sparks727@users.noreply.github.com>
2024-05-19 20:27:18 -0400

    Update route.js

--- src/app/feed.xml/route.js
+++ src/app/feed.xml/route.js
@@ -10,8 +10,8 @@ export async function GET(req) {
   }
 
   let author = {
-    name: 'Spencer Sharp',
-    email: 'spencer@planetaria.tech',
+    name: 'Alex Sparks',
+    email: 'sparksstack@gmail.com',
   }
 
   let feed = new Feed({
