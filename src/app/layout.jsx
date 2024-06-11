6ca2bbe3cca1ecb4484ecad974f2464b0029a000
Sparks727 <alex@omnilocal.co>
2024-06-11 10:52:53 -0400

    analytics1

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -29,6 +29,7 @@ export default function RootLayout({ children }) {
             <Layout>{children}</Layout>
           </div>
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
