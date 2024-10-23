0afd282f20d3f5b282b13cf2e0112978553b8c1d
Sparks727 <sparksstack@gmail.com>
2024-10-23 16:10:24 -0400

    GTM 3rd attempt

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,19 +25,27 @@ export default function RootLayout({ children }) {
     <>
       {/* Google Tag Manager */}
       <Script
-        id="google-analytics"
+        id="google-tag-manager"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
             __html: `
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MVPCHBDG');
             `
         }}
       />
       {/* End Google Tag Manager */}
       <html lang="en" className="h-full antialiased" suppressHydrationWarning>
         <body className="flex h-full bg-zinc-50 dark:bg-black">
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MVPCHBDG"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
           <Providers>
             <div className="flex w-full">
               <Layout>{children}</Layout>
