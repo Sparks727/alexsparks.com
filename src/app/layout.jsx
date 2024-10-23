96d7ceeb74a5b999672664c3fd93788d4e562ccd
Sparks727 <sparksstack@gmail.com>
2024-10-23 16:05:48 -0400

    GTM 2nd attempt

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { Analytics } from "@vercel/analytics/react"
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
-import { GoogleTagManager } from '@next/third-parties/google'; // Import GoogleTagManager
+import Script from 'next/script'
 
 import '@/styles/tailwind.css'
 
@@ -24,7 +24,18 @@ export default function RootLayout({ children }) {
   return (
     <>
       {/* Google Tag Manager */}
-      <GoogleTagManager containerId="GTM-MVPCHBDG" /> {/* Replace with your GTM container ID */}
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            `
+        }}
+      />
+      {/* End Google Tag Manager */}
       <html lang="en" className="h-full antialiased" suppressHydrationWarning>
         <body className="flex h-full bg-zinc-50 dark:bg-black">
           <Providers>
