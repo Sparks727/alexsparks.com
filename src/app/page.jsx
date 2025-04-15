1dab521ca565469fff1fb2dca80c296699767207
Sparks727 <sparksstack@gmail.com>
2025-04-15 13:41:40 -0400

    Fix: Escape apostrophe in page text

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -248,7 +248,7 @@ export default async function Home() {
             Founder & Autonomous Problem Solver
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I'm Alex Sparks, a junior web developer and entrepreneur based in Clearwater, FL. My latest project, OmniLocal, is an all-in-one software that helps businesses take control of their local online presence.
+            I&apos;m Alex Sparks, a junior web developer and entrepreneur based in Clearwater, FL. My latest project, OmniLocal, is an all-in-one software that helps businesses take control of their local online presence.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
